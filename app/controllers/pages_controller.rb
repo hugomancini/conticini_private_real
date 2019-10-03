c0223e05648311a8c39731976bc8606292c79c01
hugo mancini <h.g.mancini@gmail.com>
2019-10-03 17:31:03 +0200

    All API ok on Paper

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,14 +3,112 @@ require 'net/http'
 require 'json'
 
 class PagesController < ApplicationController
-	before_action :connectApi
-	def show
-		@product = ShopifyAPI::Product.find(params[:id])
-	end
+  before_action :connectApi
+  def show
+    @product = ShopifyAPI::Product.find(params[:id])
+  end
 
-	def index
-		@products = ShopifyAPI::Product.all
-	end
+  def index
+    @products = ShopifyAPI::Product.all
+  end
+
+
+  def getCartJson
+      hash = {"items" => [
+            {'sku' => 'noke-12345B',
+            'name' => 'Noke Sneakers',
+             'vat' => 2000,
+             'price' => 5999,
+             'quantity' => 2
+               }]}
+      @json = hash.to_json
+      return @json
+  end
+
+  def setDateTimeRecipient(checkoutId)
+    @json3 = {
+        "delivery_time" => "2018-01-28T19:43:00Z",
+        "message" => "Door code: 1234",
+        "recipient" => {
+          "first_name" => "Test",
+          "last_name" => "Testsson",
+          "address_1" => "Example street 52",
+          "address_2" => "",
+          "city" => "Paris",
+          "postcode" => "75000",
+          "phone_number" => "+331612345678",
+          "email" => "test@grr.la"
+        }
+      }
+    uri = URI.parse("https://sandbox.urb-it.com/v3/checkouts/#{checkoutId}/delivery")
+    request = Net::HTTP::Post.new(uri)
+    request.content_type = "application/json"
+    request["Authorization"] = "Bearer <JWT Authorization Header>"
+    request["X-Api-Key"] = "92012419-d73a-42f5-a12c-cdcc20740de3"
+    request.body = @json3
+    puts request.body
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+    response.code
+    answer = response.body
+    jsonAnswer = JSON.parse(answer)
+    puts "THIS IS THE API ANSWER : "
+    puts jsonAnswer
+  end
+
+  def initiateCheckOut(cartId)
+    @json2 = { "cart_reference" => @cartId }
+    uri = URI.parse("https://sandbox.urb-it.com/v3/checkouts")
+    request = Net::HTTP::Post.new(uri)
+    request.content_type = "application/json"
+    request["Authorization"] = "Bearer <JWT Authorization Header>"
+    request["X-Api-Key"] = "92012419-d73a-42f5-a12c-cdcc20740de3"
+    request.body = @json2
+    puts request.body
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+    response.code
+    answer = response.body
+    jsonAnswer = JSON.parse(answer)
+    puts "THIS IS THE API ANSWER : "
+    puts jsonAnswer
+    @checkoutId = jsonAnswer["id"]
+    setDateTimeRecipient(@checkoutId)
+  end
+
+  def initiateCart
+    getCartJson
+    puts "INITIAITIEI CAAARRTT"
+    uri = URI.parse("https://sandbox.urb-it.com/v2/carts")
+    request = Net::HTTP::Post.new(uri)
+    request.content_type = "application/json"
+    request["Authorization"] = "Bearer <JWT Authorization Header>"
+    request["X-Api-Key"] = "92012419-d73a-42f5-a12c-cdcc20740de3"
+    request.body = @json
+    puts request.body
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+    response.code
+    answer = response.body
+    jsonAnswer = JSON.parse(answer)
+    puts "THIS IS THE API ANSWER : "
+    puts jsonAnswer
+    @cartId = jsonAnswer["id"].to_s
+    initiateCheckOut(@cartId)
+  end
+
 
   def deliveryValid
     #getting info on wether the area is inside delivery area
@@ -56,9 +154,11 @@ class PagesController < ApplicationController
        puts @deliveryHours.class
        puts @deliveryHours.size
        puts @deliveryHours.first.class
+       puts "YEAYYYYYAYSYAYSYASYASYAYS"
 
         puts Date.parse(@deliveryHours.first).strftime("%a %b %e %T %Y")
        render json: {slots: @deliveryHours}
+       initiateCart
       else
     # if not, then we put an error message
        puts "Does #{params[:postcode]} can be delivered ? Answer is : #{@answer}"
@@ -68,14 +168,15 @@ class PagesController < ApplicationController
 
 
 
-	private
+
+  private
 
 
 
-	def connectApi
-		shop_url = "https://6620956a37653def370f1cb21fb82e67:f47a2189cb3f4983d295d2867accaec2@hugo-mancini.myshopify.com"
-		ShopifyAPI::Base.site = shop_url
-		ShopifyAPI::Base.api_version = '2019-07'
-	end
+  def connectApi
+    shop_url = "https://6620956a37653def370f1cb21fb82e67:f47a2189cb3f4983d295d2867accaec2@hugo-mancini.myshopify.com"
+    ShopifyAPI::Base.site = shop_url
+    ShopifyAPI::Base.api_version = '2019-07'
+  end
 
 end
