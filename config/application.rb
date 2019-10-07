4e0a0e8105c53e3c7295af5e47938ca73366f5d1
hugo mancini <h.g.mancini@gmail.com>
2019-10-07 09:52:53 +0200

    ok

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module ConticiniPrivate
    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options, :put, :patch]
       end
     end
 
